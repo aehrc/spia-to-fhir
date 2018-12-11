356a6a064ba661fc3135a718f296357335b88711
John Grimes <John.Grimes@csiro.au>
2018-12-11 14:44:27 +1000

    Update file headers

--- src/main/java/au/csiro/spiatofhir/spia/RefsetEntry.java
+++ src/main/java/au/csiro/spiatofhir/spia/RefsetEntry.java
@@ -1,7 +1,19 @@
 /*
- * Copyright CSIRO Australian e-Health Research Centre (http://aehrc.com). All rights reserved. Use is subject to
- * license terms and conditions.
+ *    Copyright 2018 Australian e-Health Research Centre, CSIRO
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
+
 package au.csiro.spiatofhir.spia;
 
 import java.util.Optional;
