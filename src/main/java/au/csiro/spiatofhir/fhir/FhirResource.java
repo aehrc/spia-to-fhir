2b65c5cc8f898cf96cf13ccce1e7b5fa599d8358
John Grimes <John.Grimes@csiro.au>
2020-08-19 19:25:35 +1000

    Update copyright notice

--- src/main/java/au/csiro/spiatofhir/fhir/FhirResource.java
+++ src/main/java/au/csiro/spiatofhir/fhir/FhirResource.java
@@ -21,16 +21,31 @@ package au.csiro.spiatofhir.fhir;
  */
 public abstract class FhirResource {
 
-  public static final String VERSION = "3.0.0";
-  public static final String COPYRIGHT =
-      "Copyright © The Royal College of Pathologists of Australasia - All rights reserved. "
-          + "This content is licensed under a Creative Commons Attribution 4.0 International "
-          + "License. See https://creativecommons.org/licenses/by/4.0/.";
-  public static final String EMAIL = "help@digitalhealth.gov.au";
-  public static final String PUBLISHER = "Australian Digital Health Agency";
-  public static final String JURISDICTION_SYSTEM = "urn:iso:std:iso:3166";
-  public static final String JURISDICTION_CODE = "AU";
-  public static final String JURISDICTION_DISPLAY = "Australia";
-  public static final String OID_SYSTEM = "urn:ietf:rfc:3986";
+    public static final String VERSION = "3.0.1";
+    public static final String COPYRIGHT = "Copyright © 2017 - 2020 RCPA. All rights reserved.\n\n" +
+            "This material contains content from LOINC (http://loinc.org). LOINC is copyright 1995-2020, " +
+            "Regenstrief Institute, Inc. and the Logical Observation Identifiers Names and Codes (LOINC) " +
+            "Committee and is available at no cost under the license at http://loinc.org/license. LOINC® is a " +
+            "registered United States trademark of Regenstrief Institute, Inc.\n\n" +
+            "This material contains information which is protected by copyright. You may download, display, " +
+            "print and reproduce any material for your personal, non-commercial use or use within your " +
+            "organisation subject to the following terms and conditions:\n\n" +
+            "1. The material may not be copied, reproduced, communicated or displayed, in whole or in part, for " +
+            "profit or commercial gain.\n" +
+            "2. Any copy, reproduction or communication must include this RCPA copyright notice in full.\n" +
+            "3. No changes may be made to the wording of the Standards and Guidelines, terminology reference " +
+            "sets and information models including commentary, tables or diagrams. Excerpts from the Standards " +
+            "and Guidelines, terminology reference sets and information models may be used. References and " +
+            "acknowledgments must be maintained in any reproduction or copy in full or part of the " +
+            "material.\n\n" +
+            "Apart from any use as permitted under the Copyright Act 1968 or as set out above, all other rights " +
+            "are reserved. Requests and inquiries concerning reproduction and rights should be addressed to " +
+            "RCPA, 207 Albion St, Surry Hills, NSW 2010, Australia.";
+    public static final String EMAIL = "help@digitalhealth.gov.au";
+    public static final String PUBLISHER = "Australian Digital Health Agency";
+    public static final String JURISDICTION_SYSTEM = "urn:iso:std:iso:3166";
+    public static final String JURISDICTION_CODE = "AU";
+    public static final String JURISDICTION_DISPLAY = "Australia";
+    public static final String OID_SYSTEM = "urn:ietf:rfc:3986";
 
 }
